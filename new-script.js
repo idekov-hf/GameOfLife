9c1f281ab94aefed35dcd4518124d17f8e164cd6
Iavor Dekov <ivdekov@gmail.com>
2016-11-04 18:49:54 -0500

    Clean up new-script.js; use proper syntax for object literal properties; add event listener for mouseup action; implement create grid and reset buttons

--- new-script.js
+++ new-script.js
@@ -1,19 +1,19 @@
 var grid = {
-	array = [],
-	rows = 0,
-	columns = 0,
-	isInitialized = false,
-	create = function(rows, columns) {
-		if (rows < 1 || columns < 1) { return; }
+	array: [],
+	rows: 0,
+	columns: 0,
+	isInitialized: false,
+	create: function(rows, columns) {
+		this.array = [];
 		this.rows = rows;
 		this.columns = columns;
 		for (var row = 0; row < rows; row++) {
 			this.array[row] = [];
 			for (var column = 0; column < columns; column++) {
 				this.array[row][column] = {
-					isAlive = false;
-					numAliveNeighbors = 0;
-				}
+					isAlive: false,
+					numAliveNeighbors: 0
+				};
 			}
 		}
 		this.isInitialized = true;
@@ -22,11 +22,11 @@ var grid = {
 		this.array.forEach(function(row, rowIndex) {
 			row.forEach(function(cell, columnIndex) {
 				cell.numAliveNeighbors = 0;
-				this.countNeighborsForCellAt(rowIndex, columnIndex);
+				this.countNeighborsForCellAtPosition(rowIndex, columnIndex);
 			}, this);
 		}, this);
 	},
-	countNeighborsForCellAt: function(row, column) {
+	countNeighborsForCellAtPosition: function(row, column) {
 		for (var i = row - 1; i <= row + 1; i++) {
 
 			if (i < 0 || i >= this.rows) { continue; }
@@ -59,50 +59,96 @@ var grid = {
 		this.updateAllCells();
 	},
 	reset: function() {
-		if (!isInitialized) { return; }
+		if (!this.isInitialized) { return; }
 		this.array.forEach(function(row) {
 			row.forEach(function(cell) {
-				cell = {
-					isAlive = false;
-					numAliveNeighbors = 0;
-				}
+				cell.isAlive = false;
+				cell.numAliveNeighbors = 0;
 			});
 		});
+	},
+	toggleCellAtPosition: function(row, column) {
+		var cell = this.array[row][column];
+		cell.isAlive = !cell.isAlive;
+	},
+	getCellAtPosition: function(row, column) {
+		return this.array[row][column];
+	},
+	log: function() {
+		var string = "";
+		this.array.forEach(function(row) {
+			string += "|";
+			row.forEach(function(cell) {
+				string += cell.isAlive ? "1|" : "0|";
+			});
+			string += "\n";
+		});
+		console.log(string);
 	}
 };
 
-var game = {
-	isPlaying = false;
-	timer;
-};
-
-var handlers = {
+var controller = {
+	isPlaying: false,
+	timer: undefined,
 	create: function() {
 		var rowsInputValue = document.getElementById("rowsInput").valueAsNumber;
 		var columnsInputValue = document.getElementById("columnsInput").valueAsNumber;
 		grid.create(rowsInputValue, columnsInputValue);
-		view.createGrid(rowsInputValue, columnsInputValue);
+		view.createTable(rowsInputValue, columnsInputValue);
 	},
 	advance: function() {
 		grid.advance();
 	},
 	start: function() {
-		if (!game.isPlaying) {
-			game.timer = setInterval(advanceGrid, 250);
+		if (!this.isPlaying) {
+			this.timer = setInterval(this.advance, 250);
 		} else {
-			clearInterval(game.timer);
+			clearInterval(this.timer);
 		}
+		this.isPlaying = !this.isPlaying;
 	},
 	reset: function() {
 		grid.reset();
+		view.resetTable();
+	},
+	toggleCell: function(td) {
+		var rowNum = parseInt(td.parentNode.id);
+		var columnNum = parseInt(td.id); 
+
+		grid.toggleCellAtPosition(rowNum, columnNum);
+		var cell = grid.getCellAtPosition(rowNum, columnNum);
+		td.className = cell.isAlive ? "filled" : "empty"
 	}
 };
 
 var view = {
-	createGrid: function(rows, columns) {
-
+	table: document.getElementById("grid"),
+	createTable: function(rows, columns) {
+		this.table.innerHTML = "";
+		for (var row = 0; row < rows; row++) {
+			var tr = this.table.insertRow(row);
+			tr.id = row;
+			for (var column = 0; column < columns; column++) {
+				var td = tr.insertCell(column);
+				td.id = column;
+			}
+		}
 	},
-	displayGrid: function() {
-
+	resetTable: function() {
+		for (row = 0; row < this.table.rows.length; row++) {
+			var tr = this.table.rows[row];
+			for (column = 0; column < tr.cells.length; column++) {
+				tr.cells[column].className = "empty";
+			}
+		}
+	},
+	setUpEventListeners: function() {
+		var table = document.getElementById("grid");
+		table.addEventListener("mouseup", function(event) {
+			var elementClicked = event.target;
+			controller.toggleCell(elementClicked);
+		});
 	}
 };
+
+view.setUpEventListeners();
