7e1130a9a9a20c0337c7e2f6b9ca2a33b24e786b
Iavor Dekov <ivdekov@gmail.com>
2016-11-04 14:58:41 -0500

    Add handlers, game and view objects

--- new-script.js
+++ new-script.js
@@ -1,9 +1,10 @@
 var grid = {
-
 	array = [],
 	rows = 0,
 	columns = 0,
+	isInitialized = false,
 	create = function(rows, columns) {
+		if (rows < 1 || columns < 1) { return; }
 		this.rows = rows;
 		this.columns = columns;
 		for (var row = 0; row < rows; row++) {
@@ -11,29 +12,97 @@ var grid = {
 			for (var column = 0; column < columns; column++) {
 				this.array[row][column] = {
 					isAlive = false;
-					aliveNeighbors = 0;
+					numAliveNeighbors = 0;
 				}
 			}
 		}
+		this.isInitialized = true;
 	},
-	countAllNeighbors: function() {
-		array.forEach(function(row, rowIndex) {
-			row.forEach(function(column, columnIndex) {
+	countNeighborsForAllCells: function() {
+		this.array.forEach(function(row, rowIndex) {
+			row.forEach(function(cell, columnIndex) {
+				cell.numAliveNeighbors = 0;
 				this.countNeighborsForCellAt(rowIndex, columnIndex);
-			});
-		});
+			}, this);
+		}, this);
 	},
 	countNeighborsForCellAt: function(row, column) {
 		for (var i = row - 1; i <= row + 1; i++) {
+
 			if (i < 0 || i >= this.rows) { continue; }
+
 			for (var j = column - 1; j <= column + 1; j++) {
+
 				if (j < 0 || j >= this.columns) { continue; }
 				if (i === row && j === column) { continue; }
 
 				if (array[i][j].isAlive) {
-					array[row][column].aliveNeighbors++;
+					array[row][column].numAliveNeighbors++;
 				}
 			}
 		}
+	},
+	updateAllCells: function() {
+		this.array.forEach(function(row) {
+			row.forEach(function(cell) {
+				var numAliveNeighbors = cell.numAliveNeighbors;
+				if (numAliveNeighbors === 3) {
+					cell.isAlive = true;
+				} else if (numAliveNeighbors < 2 || numAliveNeighbors > 3) {
+					cell.isAlive = false;
+				}
+			});
+		});
+	},
+	advance: function() {
+		this.countNeighborsForAllCells();
+		this.updateAllCells();
+	},
+	reset: function() {
+		if (!isInitialized) { return; }
+		this.array.forEach(function(row) {
+			row.forEach(function(cell) {
+				cell = {
+					isAlive = false;
+					numAliveNeighbors = 0;
+				}
+			});
+		});
+	}
+};
+
+var game = {
+	isPlaying = false;
+	timer;
+};
+
+var handlers = {
+	create: function() {
+		var rowsInputValue = document.getElementById("rowsInput").valueAsNumber;
+		var columnsInputValue = document.getElementById("columnsInput").valueAsNumber;
+		grid.create(rowsInputValue, columnsInputValue);
+		view.createGrid(rowsInputValue, columnsInputValue);
+	},
+	advance: function() {
+		grid.advance();
+	},
+	start: function() {
+		if (!game.isPlaying) {
+			game.timer = setInterval(advanceGrid, 250);
+		} else {
+			clearInterval(game.timer);
+		}
+	},
+	reset: function() {
+		grid.reset();
+	}
+};
+
+var view = {
+	createGrid: function(rows, columns) {
+
+	},
+	displayGrid: function() {
+
 	}
 };
